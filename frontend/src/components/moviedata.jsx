a0dfbea27a82f581f8752eafd5f4ae2ceb7897c8
Niklas <niklassaari04@gmail.com>
2026-09-08 20:40:30 +0300

    moviedata komponentin työstöä

--- frontend/src/components/moviedata.jsx
+++ frontend/src/components/moviedata.jsx
@@ -1,14 +1,30 @@
 import './moviedata.css';
 
 
+
 const MovieData = ({ movie }) => {
   return (
-    <div className="movie-card">
-      <h3>{movie.title}</h3>
-      <p>{movie.genre_ids}</p>
-      <p>{movie.release_date}</p>
-      <p>{movie.vote_average}</p>
+   <div className="movie">
+     
+  <div className="movie-poster">
+    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} name="logo" style={{ width: '90px', height: '150px' }} />
+  </div>
+    
+
+  <div className="movie-card">
+    
+    
+      <h3 className="movie-title">{movie.title}</h3>
+    
+    
+    <div className="movie-info">
+      <p>Genre:{movie.genre_ids}</p>
+      <p>Release date: {new Date(movie.release_date).toLocaleDateString("fi-FI")}</p>
+      <p>Rating: {movie.vote_average}</p>
     </div>
+  </div>
+  
+</div>
   );
 };
 
