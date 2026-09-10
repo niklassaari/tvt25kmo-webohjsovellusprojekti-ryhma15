fae3f0f7aec47b69d0fd1e0806e764e57d83a5c6
Niklas <niklassaari04@gmail.com>
2026-09-10 12:16:48 +0300

    Tuomon haku muutettu .jsx:ään, moviedata komponentin hiomista

--- frontend/src/components/moviedata.jsx
+++ frontend/src/components/moviedata.jsx
@@ -1,8 +1,15 @@
 import './moviedata.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useState } from 'react';
+
 
 
 
 const MovieData = ({ movie }) => {
+  
+  const [currentMovie, setCurrentMovie] = useState(null); // Added current movie state
+
   return (
    <div className="movie">
      
@@ -13,9 +20,40 @@ const MovieData = ({ movie }) => {
 
   <div className="movie-card">
     
-    
-      <h3 className="movie-title">{movie.title}</h3>
-    
+ 
+<button
+  type="button"
+  className="movie-modal-button"
+  data-bs-toggle="modal"
+  data-bs-target={`#movieModal-${movie.id}`}
+  onClick={() => setCurrentMovie(movie)}
+>
+  {movie.title}
+</button>
+
+<div className="modal fade" tabIndex="-1" id={`movieModal-${movie.id}`}>
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <img src={`https://image.tmdb.org/t/p/w500${currentMovie?.poster_path}`} name="logo" style={{ width: '90px', height: '150px' }} />
+        <h5 className="modal-title">{currentMovie?.title}</h5>
+
+        
+      </div>
+
+      <div className="movie-modal-body">
+        <p>{currentMovie?.overview}</p>
+        <p>{currentMovie?.overview || "No overview available"}</p>
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setCurrentMovie(null)}>
+          Close
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+   
     
     <div className="movie-info">
       <p>Genre:{movie.genre_ids}</p>
@@ -25,6 +63,10 @@ const MovieData = ({ movie }) => {
   </div>
   
 </div>
+
+
+
+
   );
 };
 
