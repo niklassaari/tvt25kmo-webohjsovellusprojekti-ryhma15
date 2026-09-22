33c9d4fa0ddb042bf0c4a1a784a33849f20de3fe
Niklas <niklassaari04@gmail.com>
2026-09-22 14:16:19 +0300

    konfliktien korjausta toivottavasti

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -42,7 +42,7 @@ console.log("REGISTER DATA SENT:",
   { username: newusername, email: newemail, password: newpassword 
 });
 // tuohon ip kun testaat localisti
-    const response = await fetch("http://localhost:3001/user/register", 
+    const response = await fetch("http://128.214.255.200:3001/user/register", 
       { 
       method: "POST", 
       headers: { "Content-Type": "application/json" }, 
